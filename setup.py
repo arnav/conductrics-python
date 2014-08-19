e573c450a532653b5b717f23218422e741ec3b13
Arnav Khare <arnav@yadster.com>
2014-08-19 07:21:34 +0100

    Added setup.py to install as python package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="conductrics",
+    version="0.1",
+    packages=find_packages(),
+    description="Conductrics API - Python wrapper",
+    long_description=open("README.md").read(),
+    url="https://github.com/conductrics/conductrics-python",
+)
